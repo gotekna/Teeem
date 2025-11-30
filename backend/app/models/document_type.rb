5f8d119b4d751b645e66fbedd63946ecb0e00692
RobertJHarder <robert@tekna.com.au>
2025-11-30 14:23:04 +1000

    feat: Add registry to allowed DocumentType categories

--- backend/app/models/document_type.rb
+++ backend/app/models/document_type.rb
@@ -4,7 +4,7 @@ class DocumentType < ApplicationRecord
 
   # Validations
   validates :name, presence: true, uniqueness: true
-  validates :category, inclusion: { in: %w[corporate tax compliance general financial company trust advice], allow_blank: true }
+  validates :category, inclusion: { in: %w[corporate tax compliance general financial company trust advice registry], allow_blank: true }
 
   # Scopes
   scope :active, -> { where(active: true) }
