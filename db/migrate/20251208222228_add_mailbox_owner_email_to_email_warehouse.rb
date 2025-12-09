912f19848e3965874cba365c9bf145813c88feb7
GoTekna <92342979+gotekna@users.noreply.github.com>
2025-12-09 10:24:15 +1000

    fix: Correct table name in migration (email_warehouse not email_warehouses)

--- db/migrate/20251208222228_add_mailbox_owner_email_to_email_warehouse.rb
+++ db/migrate/20251208222228_add_mailbox_owner_email_to_email_warehouse.rb
@@ -1,6 +1,6 @@
 class AddMailboxOwnerEmailToEmailWarehouse < ActiveRecord::Migration[8.0]
   def change
-    add_column :email_warehouses, :mailbox_owner_email, :string
-    add_index :email_warehouses, :mailbox_owner_email
+    add_column :email_warehouse, :mailbox_owner_email, :string
+    add_index :email_warehouse, :mailbox_owner_email
   end
 end
